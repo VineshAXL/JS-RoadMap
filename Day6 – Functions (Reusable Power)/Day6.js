32deb17bd9d9d498c1af5f268a13ee4e33f19d24
VineshAXL <csvinesh6@gmail.com>
2026-02-24 08:16:51 +0530

    day6

---
+++ Day6 – Functions (Reusable Power)/Day6.js
@@ -0,0 +1,9 @@
+// 1️⃣ Create function to find square
+function square(){
+    console.log(2*2);
+}
+square();
+// 2️⃣ Create function to find biggest of 2 numbers
+// 3️⃣ Create function to calculate simple interest
+// 4️⃣ Create function for grade calculation
+// 5️⃣ Create function for factorial

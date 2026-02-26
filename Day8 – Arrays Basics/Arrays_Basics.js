0284fe16232a1baf144d07bb4cf9aeb17b7ac2b2
VineshAXL <csvinesh6@gmail.com>
2026-02-26 21:58:59 +0530

    Array

--- Day8 – Arrays Basics/Arrays_Basics.js
+++ Day8 – Arrays Basics/Arrays_Basics.js
@@ -17,7 +17,12 @@ student.pop();
 console.log(student);
 
 // 5️⃣ Find total of numberarray
-console.log(student.length);
+let nums = [1,2,3,4,5,6,7,8,9,10]
+let total = 0;
+for(let i = 0;i<nums.length;i++){
+    total = total +nums[i];
+}
+console.log(total);
 
 // 🔥 Challenge
 
---
+++ Day9 – Advanced Array Methods (Very Important)/Advanced_Array_Methods.js
@@ -0,0 +1,47 @@
+// 💥 Practice Tasks (Compulsory)
+// 1️⃣ Double all numbers using map
+let arr  = [10,21,30,40,50];
+for (let i=0;i<=arr.length-1;i++){
+//    console.log(arr[i]*2);
+}
+
+let double =arr.map(function(eke){
+    return eke*2;
+})
+console.log(double);
+
+// 2️⃣ Filter odd numbers
+let odd = arr.filter(function (num){
+return num %2 !==0
+});
+console.log(odd);
+// 3️⃣ Convert all names to uppercase
+let lower = ['hello', 'world', 'javascript'];
+let upper = lower.map(item => item.toUpperCase());
+console.log(upper); 
+
+// 4️⃣ Find numbers greater than 50
+let nums = [20, 60, 45, 80, 30];
+let big = nums.filter(n => n > 50);
+console.log(big);
+
+// 5️⃣ Sum array using loop
+let n = 0;
+nums.forEach(nums => n+=nums);
+console.log(n);
+
+// 🔥 Challenge
+
+// Given:
+
+let students = [
+  {name: "Vinesh", marks: 80},
+  {name: "Arun", marks: 45},
+  {name: "Kumar", marks: 90}
+];
+
+// 👉 Filter students with marks above 50
+// 👉 Get only names using map
+
+let pass = students.filter(students=>students.marks>50);
+console.log(pass);
